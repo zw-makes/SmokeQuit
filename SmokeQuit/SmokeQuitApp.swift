6fdfc7576f75fd9287e2dc082694c18e314a9052
zw-makes <zwmakes@gmail.com>
2026-06-09 02:30:59 +0530

    Fix black screen: revert to SwiftUI @main, keep UIRequiresFullScreen for letterboxing fix

--- SmokeQuit/SmokeQuitApp.swift
+++ SmokeQuit/SmokeQuitApp.swift
@@ -1,15 +1,10 @@
-import UIKit
+import SwiftUI
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
-    }
-
-    func application(_ application: UIApplication,
-                     configurationForConnecting connectingSceneSession: UISceneSession,
-                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+@main
+struct SmokeQuitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomeView()
+        }
     }
 }
