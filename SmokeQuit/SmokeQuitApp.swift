87516674ebf9f40de5c87122cf1c1d26adef5c27
zw-makes <zwmakes@gmail.com>
2026-06-09 02:27:37 +0530

    Fix letterboxing: use UILaunchScreen plist + AppDelegate/SceneDelegate, remove broken storyboard

--- SmokeQuit/SmokeQuitApp.swift
+++ SmokeQuit/SmokeQuitApp.swift
@@ -1,10 +1,15 @@
-import SwiftUI
+import UIKit
 
-@main
-struct SmokeQuitApp: App {
-    var body: some Scene {
-        WindowGroup {
-            WelcomeView()
-        }
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 }
