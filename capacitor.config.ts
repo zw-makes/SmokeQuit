bb8153ae67e3d5510558e06e7e34592efc410409
zw-makes <zwmakes@gmail.com>
2026-06-09 22:55:04 +0530

    Zoom and crop app icon and web logos, removing outer border edges

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.suuquit.app',
+  appId: 'com.smokequit.app',
   appName: 'SuuQuit',
   webDir: 'dist'
 };
